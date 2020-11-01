425977c61a8560f0ce6b0e0f25887587c56fabd2
Ali Celik <aliihsancelik1@gmail.com>
2020-11-01 00:49:06 +0000

    updated

--- target/default-html-reports/report.js
+++ target/default-html-reports/report.js
@@ -35,7 +35,8 @@ formatter.match({
   "location": "com.zerobank.step_definitions.findTransactionsStepDefs.the_user_navigates_to_tab_and_module(java.lang.String,java.lang.String)"
 });
 formatter.result({
-  "status": "passed"
+  "error_message": "org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"xpath\",\"selector\":\"//a[contains(text(),\u0027Account Activity\u0027)]\"}\n  (Session info: chrome\u003d86.0.4240.111)\nFor documentation on this error, please visit: https://www.seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027aliihsancelik.local\u0027, ip: \u0027fe80:0:0:0:10cd:3e20:d6c1:6cc8%en0\u0027, os.name: \u0027Mac OS X\u0027, os.arch: \u0027x86_64\u0027, os.version: \u002710.15.4\u0027, java.version: \u002712.0.1\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 86.0.4240.111, chrome: {chromedriverVersion: 86.0.4240.22 (398b0743353ff..., userDataDir: /var/folders/_q/b9_0_0d579n...}, goog:chromeOptions: {debuggerAddress: localhost:59370}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: MAC, platformName: MAC, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:virtualAuthenticators: true}\nSession ID: 71c0446ee2f6f7e471e860d7be864de5\n*** Element info: {Using\u003dxpath, value\u003d//a[contains(text(),\u0027Account Activity\u0027)]}\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\n\tat java.base/jdk.internal.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\n\tat java.base/java.lang.reflect.Constructor.newInstanceWithCaller(Constructor.java:500)\n\tat java.base/java.lang.reflect.Constructor.newInstance(Constructor.java:481)\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:323)\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByXPath(RemoteWebDriver.java:428)\n\tat org.openqa.selenium.By$ByXPath.findElement(By.java:353)\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\n\tat com.zerobank.pages.BasePage.navigateTo(BasePage.java:15)\n\tat com.zerobank.step_definitions.findTransactionsStepDefs.the_user_navigates_to_tab_and_module(findTransactionsStepDefs.java:45)\n\tat ✽.the user navigates to \"Account Activity\" tab and \"Find Transactions\" module(file:///Users/aliihsancelik/IdeaProjects/zerobank-automation/src/test/resources/features/findTransactions.feature:7)\n",
+  "status": "failed"
 });
 formatter.step({
   "name": "the user enters date range from \"2012-09-01\" to \"2012-09-06\"",
@@ -45,7 +46,7 @@ formatter.match({
   "location": "com.zerobank.step_definitions.findTransactionsStepDefs.the_user_enters_date_range_from_to(java.lang.String,java.lang.String)"
 });
 formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.step({
   "name": "clicks find",
@@ -55,7 +56,7 @@ formatter.match({
   "location": "com.zerobank.step_definitions.findTransactionsStepDefs.clicks_find()"
 });
 formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.step({
   "name": "results table should only show transactions dates between \"2012-09-01\" to \"2012-09-06\"",
@@ -65,7 +66,7 @@ formatter.match({
   "location": "com.zerobank.step_definitions.findTransactionsStepDefs.results_table_should_only_show_transactions_dates_between_to(java.lang.String,java.lang.String)"
 });
 formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.step({
   "name": "the results should be sorted by most recent date",
@@ -75,7 +76,7 @@ formatter.match({
   "location": "com.zerobank.step_definitions.findTransactionsStepDefs.the_results_should_be_sorted_by_most_recent_date()"
 });
 formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.step({
   "name": "the user enters date range from \"2012-09-02\" to \"2012-09-06\"",
@@ -85,7 +86,7 @@ formatter.match({
   "location": "com.zerobank.step_definitions.findTransactionsStepDefs.the_user_enters_date_range_from_to(java.lang.String,java.lang.String)"
 });
 formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.step({
   "name": "clicks find",
@@ -95,7 +96,7 @@ formatter.match({
   "location": "com.zerobank.step_definitions.findTransactionsStepDefs.clicks_find()"
 });
 formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.step({
   "name": "results table should only show transactions dates between \"2012-09-02\" to \"2012-09-06\"",
@@ -105,7 +106,7 @@ formatter.match({
   "location": "com.zerobank.step_definitions.findTransactionsStepDefs.results_table_should_only_show_transactions_dates_between_to(java.lang.String,java.lang.String)"
 });
 formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.step({
   "name": "the results table should only not contain transactions dated \"2012-09-01\"",
@@ -115,8 +116,9 @@ formatter.match({
   "location": "com.zerobank.step_definitions.findTransactionsStepDefs.the_results_table_should_only_not_contain_transactions_dated(java.lang.String)"
 });
 formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
+formatter.embedding("image/png", "embedded0.png", "screenshot");
 formatter.after({
   "status": "passed"
 });

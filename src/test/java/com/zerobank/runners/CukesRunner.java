51c7f39dcc94e315427867f26c1d268fe79ea954
Ali Celik <aliihsancelik1@gmail.com>
2020-11-12 00:55:54 +0000

    updated

--- src/test/java/com/zerobank/runners/CukesRunner.java
+++ src/test/java/com/zerobank/runners/CukesRunner.java
@@ -18,7 +18,7 @@ import org.junit.runner.RunWith;
         dryRun = false, //dryRun = true won't execute the code inside the definitions, just will check and find the undefined steps!
         // if we put dryRun = false it will be normal execution, so dryRun=true helps us to earn time for finding the undefined steps!
 
-        tags = "@wip"
+        tags = "@smoke"
 
         //tags = "@smoke" //all the tags with smoke gonna execute even on a feature or on a scenario,
         //you can manage your smoke or regression suite with this way!
